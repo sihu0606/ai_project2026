734cd69ec82b31b0f0083ffd1225eaf8f0c7c054
sihu0606 <sihuhwang0915@gmail.com>
2026-05-13 13:55:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import streamlit as st
-st.title(`나의 첫 웹 서비스 만들기!`)
-a=st.text_input(`이름을 입력해주세요`)
-b=st.selectbox(`좋아하는 음식을 선택핫헤요`,[`튀김`,`행버거`,`과자`])
-if st.button(`인사말 생성`):
-  st.write(a+`님! 안녕하세요)
-  st.info(`반갑습니다`)
-  st.warning(b+`음식을 좋아하시는군요`)
-  st.error(`잘 부탁해요!`)
+st.title('나의 첫 웹 서비스 만들기!')
+a=st.text_input('이름을 입력해주세요')
+b=st.selectbox('좋아하는 음식을 선택핫헤요',['튀김','행버거','과자'])
+if st.button('인사말 생성'):
+  st.write(a+'님! 안녕하세요')
+  st.info('반갑습니다')
+  st.warning(b+'음식을 좋아하시는군요')
+  st.error('잘 부탁해요!')
   st.balloons()
   
